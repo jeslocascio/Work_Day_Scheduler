b39ffc567f619984c54570496545f35e1887a1c6
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2023-07-31 17:00:36 -0400

    Added comments to JavaScript, added images of website for use in the ReadMe, and created a profressional ReadMe file

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # Work_Day_Scheduler
+## Description
+
+When creating this project, I set out to create an interactive Work Day Scheduler in order to help people better plan out their work day. By completing this assignment, I gained a better understanding of JQuery, and the dayJS function.
+
+## Installation
+
+N/A
+
+## Usage
+
+You can access the website here: https://jeslocascio.github.io/Work_Day_Scheduler/
+
+When you open this page, you will be presented with a screen that looks like this:
+
+![The site in its base state upon opening](./assets/images/Base-Site.png)
+
+As opposed to this screenshot, the header of your screen will showcase the accurate time of whenever you a viewing the site. 
+
+Additonally, depending on the time of day you are viewing, the time slots will be one of three colors: Grey, Red, or Green. 
+
+If the time slot is Grey, that means that hour has already passed for the day. If the time slot is Red, then that means you are currently in that hour. If the time slow is Green, that means that hour is in the future.
+
+Another feature of the site is its use of text boxes and save buttons to record planned activities, as seen below:
+
+![The site with text in a time slot, and the save button being pressed](./assets/images/Save-Button-Used.png)
+
+As seen in the above image, you can click on the save button on the right side of the screen to record any typed out activies you plan to do that day. They will even still be there after you refresh the tab, or decide to close and reopen it.
+
+
+## Credits
+
+Rutgers Fullstack Flex Coding Bootcamp provided the starter code.
+
+## License
+
+MIT License
+
+Please refer to the LICENSE in the repo for more information.
+
+## Features
+
+Clock showing the accurate time.
+
+Text boxes to save planned activities.
+
+Save buttons
+
+Color-Coordinated time slots to show the time of day.
--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+//Waits until the page is fully loaded to run the following code
 $(function () {
 
+  // Updates the current time every second
   setInterval(function() {
     var today = dayjs();
     var currentTime = today.format("[It is] dddd, MMMM D, YYYY, [at] hh:mm:ss A[!]");
     $("#currentDay").text(currentTime);
   });
 
+  // Function to set colors for each hour block based on the current hour
   var setHourColors = function () {
     var currentHour = dayjs().hour();
     console.log(currentHour);
@@ -16,6 +19,7 @@ $(function () {
       var hourBlock = id.split("-")[1];
       console.log(hourBlock);
 
+       // Compares the hour block with the current hour to add appropriate classes
       if (parseInt(hourBlock) > currentHour) {
         $(this).addClass("future").removeClass("past present");
       } else if (parseInt(hourBlock) < currentHour) {
@@ -27,35 +31,38 @@ $(function () {
       
     });
   };
+  //Calls the setHourColors() function
   setHourColors();
 
- var d = new Date();
- var minute = d.getMinutes();
- var second = d.getSeconds();
+  // Get the current minute and second
+  var d = new Date();
+  var minute = d.getMinutes();
+  var second = d.getSeconds();
 
- if (minute !== 0 || second !== 0) {
-  var timeToRefresh = (60 - minute) * 60000 + (60 - second) * 1000;
-  setTimeout(function () {
+  // Refreshes the page whenever the minute and second of the day is 0 (effectively, it refreshes the page every hour ON the hour)
+  if (minute !== 0 || second !== 0) {
+    var timeToRefresh = (60 - minute) * 60000 + (60 - second) * 1000;
+    setTimeout(function () {
+      location.reload();
+    }, timeToRefresh);
+  } else {
+    console.log("Time to refresh!");
     location.reload();
-  }, timeToRefresh);
-} else {
-  console.log("Time to refresh!");
-  location.reload();
-}
+  };
 
-
-$(".saveBtn").on("click", function () {
-  var block = $(this).parent().attr("id");
-  var task = $(this).siblings(".description").val().trim();
-  localStorage.setItem(block, task);
-});
-
-
-$(function () {
-  $(".time-block").each(function () {
-    var block = $(this).attr("id");
-    $(this).children(".description").val(localStorage.getItem(block));
+  //Saves the typed task to local storage whenever the save button is clicked
+  $(".saveBtn").on("click", function () {
+    var block = $(this).parent().attr("id");
+    var task = $(this).siblings(".description").val().trim();
+    localStorage.setItem(block, task);
   });
-});
+
+// When the page is loaded, it displays the saved tasks from local storage in each time block
+  $(function () {
+    $(".time-block").each(function () {
+      var block = $(this).attr("id");
+      $(this).children(".description").val(localStorage.getItem(block));
+    });
+  });
 
 });
